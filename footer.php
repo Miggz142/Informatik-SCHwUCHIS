da78ce882e6e5cc2a9147d6e7258910f271af9c9
kanye east 88 is my name <iljastr777@gmail.com>
2025-05-12 15:00:12 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Diese Datei wird am Ende jeder Seite eingebunden und dient dazu, valides HTML auszugeben.
- * Du kannst hier aber auch einen angepassten Footer einfügen, der z.B. eine Menüführung enthält oder Kontaktdaten anzeigt.
+  (Footer einfügen, z.B. eine Menüführung  Kontaktdaten anzeigen)
  */
 ?>
 
