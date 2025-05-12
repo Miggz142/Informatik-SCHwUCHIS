f5e3daf8f46beb0e8e88f55e910e9a947cb24700
XXL Johnson <blue.land303@gmail.com>
2025-05-12 14:22:33 +0200

    Framework
    Das Original Framework von Herr Dobrunz

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * In dieser Datei werden alle Konfigurationen für die Webseite vorgenommen.
+ * Dazu gehören die Datenbankverbindung und die Sessionkonfiguration.
+ * Hier musst du deine eigenen Datenbankdaten eintragen.
+ */
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'cu-freieevangelischeschule01_dob');
+define('DB_PASS', 'qb$XBqo74H6o&u');
+define('DB_NAME', 'cu-freieevangelischeschule01_dob');
+
+// Sessionkonfiguration
+ini_set('session.gc_maxlifetime', 3600); // Session läuft nach 1 Stunde ab
+session_set_cookie_params(3600); // Cookie läuft ebenfalls nach 1 Stunde ab
+session_start();
+
+?>
